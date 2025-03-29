3efcb0e63a4ba28007b117edb098ec10c8288a45
etoevelina <evelina.penkova0@gmail.com>
2025-03-29 17:38:26 +0500

    Initial Commit

---
+++ AgroTrack/AgroTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgroTrackApp.swift
+//  AgroTrack
+//
+//  Created by Эвелина Пенькова on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AgroTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
